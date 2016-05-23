366e9d26f894ea9a7d5941cb10de7545e17f99ce
Maumov <mau.movilla@gmail.com>
2016-05-22 21:46:18 -0500

    Starting shooter repository

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    float Vertical, Horizontal, MouseX, MouseY;
+    Vector3 direction;
+    GameObject cameraObject;
+    CharacterController characterController;
+
+    public float Speed = 1f; 
+    public bool InvertMouse;
+	// Use this for initialization
+	void Start () {
+        cameraObject = gameObject.GetComponentInChildren<Camera>().gameObject;
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetInputs();
+        cameraObject.transform.Rotate(MouseY,0f,0f);
+        transform.Rotate(0f,MouseX,0f);
+        direction.z = Vertical;
+        direction.x = Horizontal;
+        direction  = transform.rotation * direction;
+        characterController.Move(direction * Speed * Time.deltaTime);
+	}
+    void GetInputs() {
+        Vertical = Input.GetAxis("Vertical");
+        Horizontal = Input.GetAxis("Horizontal");
+        MouseX = Input.GetAxis("Mouse X");
+        MouseY = InvertMouse ? Input.GetAxis("Mouse Y"): -1f * Input.GetAxis("Mouse Y");
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+    Camera camera;
+    bool fire1;
+
+    Ray ray;
+    RaycastHit hit;
+    // Use this for initialization
+    void Start()
+    {
+
+        camera = gameObject.GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        if (fire1)
+        {
+            Fire();
+        }
+
+    }
+    void GetInputs()
+    {
+        fire1 = Input.GetButtonDown("Fire1");
+    }
+    void Fire()
+    {
+        ray = camera.ScreenPointToRay(new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f));
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Shooted " + hit.collider.name);
+            Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
+            if (rb != null) {
+                rb.AddForce(ray.direction * 15f,ForceMode.Impulse);
+            }
+        }
+        else {
+            Debug.Log("Miss");
+        }
+        
+    }
+}
